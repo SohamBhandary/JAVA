1eef44c4a450ff2da0cf86f67feb70fc9346f66e
Soham Bhandary <142657773+SohamBhandary@users.noreply.github.com>
2025-03-05 16:46:44 +0530

    Initial Commit

---
+++ Literals.java
@@ -0,0 +1,11 @@
+public class Literals {
+    public static void main(String[] args) {
+        int num=10_0000_000;
+        System.out.println(num);
+        double f=56;
+        System.out.println(f);
+        char a='s';
+        a++;
+        System.out.println(a);
+    }
+}
