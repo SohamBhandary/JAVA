1f8eaa311a3441688f34624549a6be20d5e8c614
Soham Bhandary <142657773+SohamBhandary@users.noreply.github.com>
2025-03-06 17:54:39 +0530

    Initial Commit

---
+++ Arithmetic_Ops.java
@@ -0,0 +1,17 @@
+public class Arithmetic_Ops {
+    public static void main(String[] args) {
+        int n=1;
+        int m=2;
+        int v=m+2;
+        v++;
+        v--;
+
+        System.out.println(v);
+        System.out.println(n+m);
+        System.out.println(n-m);
+        System.out.println(n*m);
+        System.out.println(n/m);
+
+
+    }
+}
