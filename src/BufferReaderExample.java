6c1af30731c63f6dd9000d9decb17cc562f530a9
SohamBhandary <bhandarysoham27@gmail.com>
2025-03-24 16:09:40 +0530

    Initial Commit

---
+++ src/BufferReaderExample.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class BufferedReaderExample {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your name: ");
+        String name = reader.readLine();
+
+        System.out.print("Enter your age: ");
+        int age = Integer.parseInt(reader.readLine());
+
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+    }
+}
---
+++ src/UserInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter any number: ");
+        int n = sc.nextInt();
+        System.out.println("your entered number:"+n);
+
+    }
+}
